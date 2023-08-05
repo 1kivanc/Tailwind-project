8c98e780ff1617f66cdfe4914b71f08bcb0a158f
1kivanc <kivanc0556@gmail.com>
2023-08-05 13:09:51 +0300

    google-font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+    },
+    extend: {
+      fontFamily:{
+        poppins: ['Poppins', 'sans-serif'],
+        opensans: ['Open Sans', 'sans-serif'],
+        roboto:['Roboto', 'sans-serif']
+      },
+
+      colors: {
+        'Tred': "#BC1A45",
+        'Tmelon':"#FFD369",
+        'Tgrey':"#DDDDDD",
+        'Twhite':"#F7F7F7"
+      }
+    },
   },
   plugins: [],
 }
